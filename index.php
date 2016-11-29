b8894967bd2508dc023d41368456bd3d6ee0e509
chayapon1219 <p.chayapon@hotmail.com>
2016-11-30 03:24:43 +0700

    Add order to generate files

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header('Content-Type: text/html; charset=utf-8'); //if this shows error then rem
 include'fb.php';
 
 // $facebook_id = '100013243178052';
-$url_user = 'chayapon.saepung';
+$url_user = array("ji.peanvidhayasakul","namfah.phannipha","chayapon.saepung");
 $is_last_page = false;
 $start_index = 1;
 $result = array();
@@ -15,71 +15,81 @@ $categories_name = array("Activity","Interested","Music","Book","Movie","TV Show
 $count_like_pages = 0;
 $count_category_pages = 0;
 
-echo 'Page that user id = ['.$url_user.'] likes <br>';
-echo '======================================================== <br>';
 
-for ($i = 0; $i <= sizeof($categories_likes)-1; $i++) {
-	$start_index = 1;
-	$count_category_pages = 0;
+for ($numUser = 0; $numUser <= sizeof($url_user)-1; $numUser++) {
+	//Create empty array
+	$result = array();
+	$count_like_pages = 0;
 
-	echo '********** Section ID = ' .$categories_name[$i]. '[' .$categories_likes[$i].']' . ' **********' . '<br>';
-	 while(!$is_last_page) {
-		$url = 'https://mbasic.facebook.com/'.$url_user.'?v=likes&sectionid='.$categories_likes[$i].'&startindex='.$start_index.'&_rdr';
-		curl_setopt($ch, CURLOPT_URL, $url);
-		$demo_mac = curl_exec($ch);
-		$html_source = htmlspecialchars($demo_mac);
-		$pages_ids = $html_source;
+	//Head of progress
+	echo 'Page that user id = ['.$url_user[$numUser].'] likes <br>';
+	echo '======================================================== <br>';
 
-		for($x = 0; $x <= 10; $x++) {
-			$pages_ids = substr($pages_ids, strpos($pages_ids, 'fan&amp;')+15);
-			$pages_id = substr($pages_ids, 0, strpos($pages_ids, '&amp;'));
+	for ($i = 0; $i <= sizeof($categories_likes)-1; $i++) {
+		$start_index = 1;
+		$count_category_pages = 0;
 
-			//Is the last page --> end loop
-			if(strlen($pages_id) > 30) {
-				goto end;
+		//echo '********** Section ID = ' .$categories_name[$i]. '[' .$categories_likes[$i].']' . ' **********' . '<br>';
+		 while(!$is_last_page) {
+			$url = 'https://mbasic.facebook.com/'.$url_user[$numUser].'?v=likes&sectionid='.$categories_likes[$i].'&startindex='.$start_index.'&_rdr';
+			curl_setopt($ch, CURLOPT_URL, $url);
+			$demo_mac = curl_exec($ch);
+			$html_source = htmlspecialchars($demo_mac);
+			$pages_ids = $html_source;
+
+			for($x = 0; $x <= 10; $x++) {
+				$pages_ids = substr($pages_ids, strpos($pages_ids, 'fan&amp;')+15);
+				$pages_id = substr($pages_ids, 0, strpos($pages_ids, '&amp;'));
+
+				//Is the last page --> end loop
+				if(strlen($pages_id) > 30) {
+					goto end;
+				}
+
+				if (strlen($pages_id) > 5 && ord($pages_id) < 58) { //Fix some error page id
+					//Print page id that this users likes
+					$count_category_pages++;
+					$count_like_pages += 1;  
+					$result[$count_like_pages] = $pages_id;
+					//echo $pages_id;
+				}
 			}
+			$start_index += 11;
+		 }
 
-			if (strlen($pages_id) > 5 && ord($pages_id) < 58) { //Fix some error page id
-				//Print page id that this users likes
+		 end: //Get the first page that this url cannot show (must use specific URL)
+			$url = 'https://mbasic.facebook.com/'.$url_user[$numUser].'?v=likes&sectionid='.$categories_likes[$i].'&startindex=-20000&_rdr';
+			curl_setopt($ch, CURLOPT_URL, $url);
+			$demo_mac = curl_exec($ch);
+			$html_source = htmlspecialchars($demo_mac);
+			$pages_ids = $html_source;
+		 	if (strpos($pages_ids, 'fan&amp;') != null) {
+				$pages_ids = substr($pages_ids, strpos($pages_ids, 'fan&amp;')+15);
+				$pages_id = substr($pages_ids, 0, strpos($pages_ids, '&amp;'));
 				$count_category_pages++;
-				$count_like_pages += 1;  
+				$count_like_pages += 1;
 				$result[$count_like_pages] = $pages_id;
 				//echo $pages_id;
+				//echo '----------> [ ' . $count_category_pages . ' ] Pages <br><br>';
+			} else {
+				//echo '----------> not have pages in this category <br><br>';
 			}
-		}
-		$start_index += 11;
-	 }
+	}
 
-	 end: //Get the first page that this url cannot show (must use specific URL)
-		$url = 'https://mbasic.facebook.com/'.$url_user.'?v=likes&sectionid='.$categories_likes[$i].'&startindex=-20000&_rdr';
-		curl_setopt($ch, CURLOPT_URL, $url);
-		$demo_mac = curl_exec($ch);
-		$html_source = htmlspecialchars($demo_mac);
-		$pages_ids = $html_source;
-	 	if (strpos($pages_ids, 'fan&amp;') != null) {
-			$pages_ids = substr($pages_ids, strpos($pages_ids, 'fan&amp;')+15);
-			$pages_id = substr($pages_ids, 0, strpos($pages_ids, '&amp;'));
-			$count_category_pages++;
-			$count_like_pages += 1;
-			$result[$count_like_pages] = $pages_id;
-			//echo $pages_id;
-			//echo '----------> [ ' . $count_category_pages . ' ] Pages <br><br>';
-		} else {
-			//echo '----------> not have pages in this category <br><br>';
-		}
+	echo 'This user likes  ' .$count_like_pages. '  Pages';
+	//echo 'This array = ' . count($result) . ' Number' ;
 
+	//Change array to Json file
+	$result = array_values($result);
+	$result = json_encode($result);
+
+	//Write files
+	file_put_contents($url_user[$numUser] .'.json', $result);
+
+	//Response
+	echo '<br> Complete file';
+	echo '<br>  ======================================================== <br><br>';
 }
 
-echo '======================================================== <br>';
-echo 'This user likes  ' .$count_like_pages. '  Pages' . '<br>'. '<br>';
-echo 'This array = ' . count($result) . ' Number' ;
+echo 'all section complete';
 
-//Change array to Json file
-$result = array_values($result);
-$result = json_encode($result);
-
-//Write files
-file_put_contents('Result'.'.json', $result);
-
-//Response
-echo '<br> Complete file';
